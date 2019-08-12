2558330fa867cba3813e28c998d6027d296b43e4
Thiago <thiago__rocha17@hotmail.com>
2019-08-12 15:55:45 -0300

    Botao Cadastro Salva o Login e Senha num txt (Basicao ainda)

--- src/SistemaLogin.java
+++ src/SistemaLogin.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class SistemaLogin {
+    Scanner leia = new Scanner(System.in);
+
     public SistemaLogin(){
         JFrame tela = new JFrame("Sistema Login");
         JPanel panel = new JPanel();
@@ -13,9 +18,9 @@ public class SistemaLogin {
         JButton cadastrar = new JButton("Cadastrar");
         JButton logar = new JButton("Logar!");
 
-
         tela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         tela.setSize(250,100);
+        tela.setResizable(false);
 
         panel.setLayout(new GridLayout(3,1));
         panel.add(lblUsuario);
@@ -25,10 +30,25 @@ public class SistemaLogin {
         panel.add(cadastrar);
         panel.add(logar);
 
+        cadastrar.addActionListener(actionEvent -> {
+            String nome = txfUsuario.getText();
+            String senha = String.valueOf(psfSenha.getPassword());
+            cadastro(nome,senha);
+        });
 
         tela.add(panel,BorderLayout.CENTER);
         tela.setVisible(true);
     }
+
+    void cadastro(String nome, String senha){
+        try {
+            PrintStream escrever = new PrintStream("logins.txt");
+            escrever.print(nome + " " + senha);
+        }catch(FileNotFoundException e){
+            System.out.println("Erro, Arquivo nao encontrado!");
+        }
+    }
+
     public static void main(String[] args) {
         new SistemaLogin();
     }
